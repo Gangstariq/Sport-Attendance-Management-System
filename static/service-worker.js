90f641911a048c0aacac636474d6c9b70770511d
Tariq <tariq@moollan.net>
2025-07-21 21:08:15 +1000

    -Added a manifest json and service worker to make this a pwa

---
+++ static/service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "sportmanager-cache-v1";
+const STATIC_ASSETS = [
+    "/",
+    "/static/style.css",
+    "/static/Check_192_icon.png",
+    "/static/Check_512_icon.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(STATIC_ASSETS);
+        })
+    );
+});
+
+// Fetch from cache or network
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate new cache and remove old
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
+        })
+    );
+});
